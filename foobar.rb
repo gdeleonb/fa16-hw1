cb2c64922a18fe4b55210c4a8033c7ba0739399d
nazgus <gus@deleon.org.mx>
2016-09-20 19:28:51 -0700

    Defined self.baz (and I hate myself a little

--- foobar.rb
+++ foobar.rb
@@ -1,9 +1,5 @@
 class Foobar
-
   def self.baz(a)
-    # Class method
-    # Call with `Foobar.baz`
+     a.map { |e| e.to_i}.map { |e| e + 2 }.delete_if { |e| e % 2 != 0 }.uniq.delete_if { |e| e > 10 }.inject(0) { |sum, e| sum + e }
   end
 end
-
-
